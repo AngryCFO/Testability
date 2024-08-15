88b69ca2a7c66b12ae31aa29f23bfbb1e0f83c04
AngryCFO <a79955700980@gmail.com>
2024-08-16 01:52:03 +0300

    Initial commit

---
+++ src/Case1/BonusMilesService.java
@@ -0,0 +1,9 @@
+package Case1;
+
+public class BonusMilesService {
+    public int calculate(int price) {
+        return price / 20;
+    }
+}
+
+
---
+++ src/Case2/BmiService.java
@@ -0,0 +1,8 @@
+package Case2;
+
+public class BmiService {
+    public int calculate(double heightInMeters, double weightInKilograms) {
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+        return (int) bmi;
+    }
+}
